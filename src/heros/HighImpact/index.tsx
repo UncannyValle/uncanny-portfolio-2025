3b4eeb18b03e9b2cc3e0acf4832ab1d995999146
uncannyvalle <jvalle0926@gmail.com>
2025-08-23 17:58:08 -0400

    feat: add "Hire Me" button with styling updates for HighImpact component

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -20,7 +20,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       className="relative -mt-[10.4rem] flex items-center justify-center text-white max-h-vh height-full"
       data-theme="dark"
     >
-      <div className="container pt-[10.4rem] mb-8 z-10 relative flex items-center justify-center">
+      <div className="container pt-[10.4rem] mb-8 z-10 relative flex items-center justify-center border-b-amber-600">
         <div className="max-w-[36.5rem] md:text-center">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
@@ -34,6 +34,14 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
               })}
             </ul>
           )}
+          <div className="mt-8 flex md:justify-center justify-start">
+            <div
+              className="contra-hire-me-button"
+              data-analyticsuserid="bdd1f6e6-a9f3-40ad-b8d4-fcf2df519c9d"
+              data-theme="dark"
+              data-username="julian_valle"
+            />
+          </div>
         </div>
       </div>
       <div className="min-h-[80vh] select-none">
