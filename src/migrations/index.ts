04cca189aeb7a28be925925fb4c14e85836be3a6
uncannyvalle <jvalle0926@gmail.com>
2025-09-08 18:31:32 -0400

    feat: add full schema migration for `pages`, `page blocks`, and associated content structures

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250908_201529_add_prefix_field_to_media from './20250908_201529_add_prefix_field_to_media';
+import * as migration_20250908_222918 from './20250908_222918';
 
 export const migrations = [
   {
     up: migration_20250908_201529_add_prefix_field_to_media.up,
     down: migration_20250908_201529_add_prefix_field_to_media.down,
-    name: '20250908_201529_add_prefix_field_to_media'
+    name: '20250908_201529_add_prefix_field_to_media',
+  },
+  {
+    up: migration_20250908_222918.up,
+    down: migration_20250908_222918.down,
+    name: '20250908_222918'
   },
 ];
