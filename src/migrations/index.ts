901ad0241f5428e699cef3647cca95a20fce6faf
uncannyvalle <jvalle0926@gmail.com>
2025-09-08 20:08:09 -0400

    chore: delete unused full schema migration file affecting `pages` and `page blocks` tables

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250908_225218 from './20250908_225218';
-
-export const migrations = [
-  {
-    up: migration_20250908_225218.up,
-    down: migration_20250908_225218.down,
-    name: '20250908_225218'
-  },
-];
