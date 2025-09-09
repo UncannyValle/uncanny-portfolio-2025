4b6ac6afbf7964297f513803958a872095b5273c
uncannyvalle <jvalle0926@gmail.com>
2025-09-08 21:33:24 -0400

    feat: add schema migration for `pages`, `page blocks`, and related entities

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250909_012842 from './20250909_012842';
+import * as migration_20250909_013249 from './20250909_013249';
 
 export const migrations = [
   {
     up: migration_20250909_012842.up,
     down: migration_20250909_012842.down,
-    name: '20250909_012842'
+    name: '20250909_012842',
+  },
+  {
+    up: migration_20250909_013249.up,
+    down: migration_20250909_013249.down,
+    name: '20250909_013249'
   },
 ];
